7b6aa36ee04d28062365481f004010d165042b39
dagmawibabi <1babidagi@gmail.com>
2024-06-07 03:55:03 +0300

    app done

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+import 'package:dio/dio.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var urlBase = "https://api.daveget.tech/api/";
+  void sendQRToAPI(code) async {
+    final dio = Dio();
+    var response =
+        await dio.post("$urlBase/Laborers/RFID", data: {'employeeID': 1234});
+  }
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      print(result?.code);
+      // sendQRToAPI(result?.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Attendance Scanner",
+        ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: QRView(
+                  key: qrKey,
+                  onQRViewCreated: _onQRViewCreated,
+                  formatsAllowed: const [BarcodeFormat.qrcode],
+                ),
+              ),
+              // Expanded(
+              //   flex: 1,
+              //   child: Center(
+              //     child: (result != null)
+              //         ? Text('Data: ${result!.code}')
+              //         : Text('Scan a code'),
+              //   ),
+              // ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Scan",
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
